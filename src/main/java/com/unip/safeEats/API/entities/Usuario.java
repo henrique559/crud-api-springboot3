9cc3eb6cf22521f648f5163e14207d7a3450b954
henrique559 <pe.henrique1981@gmail.com>
2024-11-01 17:38:49 -0300

    entidades e resources adicionados

---
+++ src/main/java/com/unip/safeEats/API/entities/Usuario.java
@@ -0,0 +1,67 @@
+package com.unip.safeEats.API.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Usuario implements Serializable {
+    private Integer idUsuario;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer idUsuario, String nome, String email, String senha) {
+        this.idUsuario = idUsuario;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(idUsuario, usuario.idUsuario);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(idUsuario);
+    }
+}
